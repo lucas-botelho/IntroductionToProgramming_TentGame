d563204253ecdfe63828dd2357993eef4d3c2601
Lucas Botelho <l_botelho@hotmail.com>
2022-11-26 17:05:51 +0000

    fun validaDataNascimento diferente dos outros rapazes

--- src/Main.kt
+++ src/Main.kt
@@ -1,28 +1,32 @@
 fun main() {
-    var userOption :Int?
-
+    var userOption: Int?
+    var allUserInputsAreValid = true
     do {
         userOption = DrawMenu()
 
-        println("Qual a sua data de nascimento? (dd-mm-yyyy)")
-        val birthDate = readln()
+        if (userOption == 1) {
+            println("Qual a sua data de nascimento? (dd-mm-yyyy)")
+            val birthDate = readln()
+            var birthDateText = validaDataNascimento(birthDate)
 
-        var birthDateText = validaDataNascimento(birthDate)
+            //!= null means it's not a valid date
+            if (birthDateText != null) {
+                println(birthDateText)
+                allUserInputsAreValid = false
+            }
 
-        if (birthDateText != null) {
-            println(birthDateText)
+            if (allUserInputsAreValid) {
+                val totalLines = GetUserBoardRowsSettings("Quantas linhas?", "Opcao invalida\n")
+                val totalColumns = GetUserBoardRowsSettings("Quantas colunas?", "Opcao invalida\n")
+                println(criaTerreno(totalLines, totalColumns))
+            }
         }
 
-    }while (birthDateText != null)
-
-    if (userOption == 1) {
-        val totalLines = GetUserBoardRowsSettings("Quantas linhas?", "Opcao invalida\n")
-        val totalColumns = GetUserBoardRowsSettings("Quantas colunas?", "Opcao invalida\n")
-       println(criaTerreno(totalLines, totalColumns))
-    }
+        allUserInputsAreValid = allUserInputsAreValid || userOption == 0
+    } while (!allUserInputsAreValid)
 }
 
-fun DrawMenu() : Int? {
+fun DrawMenu(): Int? {
     var userOption: Int?
     var isInvalidInput = false
 
@@ -39,6 +43,7 @@ fun DrawMenu() : Int? {
 
     return userOption
 }
+
 fun criaMenu(): String {
     return "\nBem vindo ao jogo das tendas\n\n" + "1 - Novo jogo\n" + "0 - Sair\n"
 }
@@ -65,31 +70,32 @@ fun criaTerreno(numLinhas: Int, numColunas: Int, mostraLegendaHorizontal: Boolea
     var lineCount = 1
     var columnCount = 1
     var boardText = ""
+    val tentChar = '\u25B3'
 
     if (mostraLegendaHorizontal) {
         boardText += criaLegendaHorizontal(numColunas)
     }
 
     while (lineCount <= numLinhas) {
-        columnCount = 1
+        columnCount = 0
         var isSingleDigitLine = lineCount in 1..9
 
         while (columnCount <= numColunas) {
-            var isFirstColumn = columnCount == 1
-
-            boardText += if (isFirstColumn) {
-                if (isSingleDigitLine) {
-                    " ${lineCount}|"
-                } else {
-                    "${lineCount}|"
+            var isFirstColumn = columnCount == 0
+            var isLastColumn = columnCount == numColunas
+            if (isFirstColumn) {
+                boardText += when(isSingleDigitLine){
+                    true -> " ${lineCount}|"
+                    false -> "${lineCount}|"
                 }
             } else {
-                "   |"
+                boardText += when(isLastColumn){
+                    true -> " $tentChar"
+                    false -> "   |"
+                }
             }
-
             columnCount++
         }
-
         boardText += "\n"
         lineCount++
     }
@@ -102,8 +108,8 @@ fun criaLegendaHorizontal(numColunas: Int): String {
     var legendaHorizontal = "  "
     var rowChar = 'A'
 
-    while (columnCount <= numColunas){
-         legendaHorizontal += "| $rowChar "
+    while (columnCount <= numColunas) {
+        legendaHorizontal += "| $rowChar "
 
         rowChar++
         columnCount++
@@ -111,26 +117,26 @@ fun criaLegendaHorizontal(numColunas: Int): String {
     return "$legendaHorizontal\n"
 }
 
-fun validaDataNascimento(data: String?) : String?{
+fun validaDataNascimento(data: String?): String? {
     var result: String? = "Data invalida"
 
-    if(data != null && data.length == 10){
-        val day = "${data[0]}${data[1]}".toInt()
-        val month = "${data[3]}${data[4]}".toInt()
-        val year = "${data[6]}${data[7]}${data[8]}${data[9]}".toInt()
+    if (data != null && data.length == 10) {
+        val day = "${data[0]}${data[1]}".toIntOrNull()
+        val month = "${data[3]}${data[4]}".toIntOrNull()
+        val year = "${data[6]}${data[7]}${data[8]}${data[9]}".toIntOrNull()
 
         var isCorrectDateFormat = data[2] == '-' && data[5] == '-'
-        if(!isCorrectDateFormat) {
+        if (!isCorrectDateFormat || day == null || month == null || year == null) {
             return result
         }
 
         val isUnderAge = year <= 2004 && month >= 11
-        if(isUnderAge){
-            isCorrectDateFormat = false
+        if (isUnderAge) {
             result = "Menor de idade nao pode jogar"
         }
 
-        val isThirtyOneDaysMonth = month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12
+        val isThirtyOneDaysMonth =
+            month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12
         if (isThirtyOneDaysMonth) {
             if (day in 1..31) {
                 isCorrectDateFormat = true
@@ -138,7 +144,7 @@ fun validaDataNascimento(data: String?) : String?{
         }
 
         val isFebruary = month == 2
-        if (isFebruary){
+        if (isFebruary) {
             val isLeapYear = ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
             if (isLeapYear && day in 1..29) {
                 isCorrectDateFormat = true
@@ -150,10 +156,10 @@ fun validaDataNascimento(data: String?) : String?{
 
         val isThirtyDaysMonth = month == 4 || month == 6 || month == 9 || month == 11
         if (isThirtyDaysMonth && day in 1..30) {
-                isCorrectDateFormat = true
+            isCorrectDateFormat = true
         }
 
-        if(isCorrectDateFormat){
+        if (isCorrectDateFormat) {
             result = null
         }
     }
